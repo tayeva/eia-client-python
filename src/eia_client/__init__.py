b590e27fe73abf8eba93b7c3213f433211a82a25
tayeva <124453543+tayeva@users.noreply.github.com>
2023-02-09 14:17:23 -0800

    sort

--- src/eia_client/__init__.py
+++ src/eia_client/__init__.py
@@ -4,8 +4,8 @@ __version__ = "0.1.0"
 __author__ = "tayeva"
 
 
-from eia_client import client
 from eia_client import api_key
+from eia_client import client
+from eia_client import parse
 from eia_client.api_key import ApiKey
 from eia_client.endpoint import Endpoint, EndpointBuilder
-from eia_client import parse
