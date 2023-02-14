73b1acf67d7daba6364b78c59178245e96fb2f87
tayeva <124453543+tayeva@users.noreply.github.com>
2023-02-13 20:27:13 -0800

    update unit tests for new changes

--- tests/test_api_endpoint_builder.py
+++ tests/test_api_endpoint_builder.py
@@ -1,27 +1,49 @@
-"""Test eia_client.api_endpoint module."""
+"""Test eia_client.api_endpoint module.
 
-from eia_client.endpoint import EndpointBuilder
+TODO: finish updating these 
+"""
+from dataclasses import asdict
+
+from eia_client.endpoint import TotalEnergy
 from eia_client.api_key import ApiKey
 
 
-def test_total_energy():
+def test_total_energy_endpoint():
     """Test the total energy endpoint with no arguments"""
-    builder = EndpointBuilder(ApiKey("TEST"))
-    endpoint = builder.total_energy()
-    true_endpoint = ("https://api.eia.gov/v2/total-energy/data/?"
-                     "api_key=TEST&frequency=monthly&data[0]=value"
-                     "&facets[msn][]=ELEPTUS&sort[0][column]=period"
-                     "&sort[0][direction]=desc&offset=0&length=5000")
+    endpoint = TotalEnergy(api_key=ApiKey("TEST")).build()
+    true_endpoint = "https://api.eia.gov/v2/total-energy/data/?api_key=TEST"
     assert endpoint.endpoint == true_endpoint
+
+
+def test_total_energy_endpoint_defautl_params():
+    """Test the total energy endpoint with no arguments"""
+    endpoint = TotalEnergy(api_key=ApiKey("TEST")).build()
+    params = asdict(endpoint.params)
+    assert params == {
+        "frequency": "monthly",
+        "data": ["value"],
+        "facets": {"msn": ["ELETPUS"]},
+        "start": None,
+        "end": None,
+        "sort": [{"column": "period", "direction": "desc"}],
+        "offset": 0,
+        "length": 5000,
+    }
 
 
 def test_total_energy_start_end():
     """Test the total energy endpoint with no arguments"""
-    builder = EndpointBuilder(ApiKey("TEST"))
-    endpoint = builder.total_energy(start="2020-01", end="2022-01")
-    true_endpoint = ("https://api.eia.gov/v2/total-energy/data/"
-                     "?api_key=TEST&frequency=monthly&data[0]=value"
-                     "&facets[msn][]=ELEPTUS&sort[0][column]=period"
-                     "&sort[0][direction]=desc&offset=0&length=5000"
-                     "&start=2020-01&end=2022-01")
-    assert endpoint.endpoint == true_endpoint
+    endpoint = TotalEnergy(
+        api_key=ApiKey("TEST"), start="2020-01", end="2022-01"
+    ).build()
+    params = asdict(endpoint.params)
+    assert params == {
+        "frequency": "monthly",
+        "data": ["value"],
+        "facets": {"msn": ["ELETPUS"]},
+        "start": "2020-01",
+        "end": "2022-01",
+        "sort": [{"column": "period", "direction": "desc"}],
+        "offset": 0,
+        "length": 5000,
+    }
