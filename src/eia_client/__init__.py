c19fafa5edfe807c1258b6c9d1662bb99d9ca9df
tayeva <124453543+tayeva@users.noreply.github.com>
2023-02-13 19:56:57 -0800

    update imports to modules and Client class

--- src/eia_client/__init__.py
+++ src/eia_client/__init__.py
@@ -4,7 +4,6 @@ __version__ = "0.2.0"
 
 
 from eia_client import api_key
-from eia_client.client import Client
+from eia_client import endpoint
 from eia_client import parse
-from eia_client.api_key import ApiKey
-from eia_client.endpoint import Endpoint, EndpointBuilder
+from eia_client.client import Client
