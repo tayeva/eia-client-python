a8598f20f1557e34faa2bc76debdb40717c56a34
tayeva <124453543+tayeva@users.noreply.github.com>
2023-02-14 11:16:37 -0800

    include a data field

--- examples/example_elec_retail_sales.py
+++ examples/example_elec_retail_sales.py
@@ -5,7 +5,9 @@ import eia_client as ec
 
 def main():
     """Example electricity retail sales."""
-    endpoint = ec.endpoint.ElectricityRetailSales(state=["CA", "NY"], sector="RES")
+    endpoint = ec.endpoint.ElectricityRetailSales(
+        data="price", state=["CA", "NY"], sector="RES"
+    )
     client = ec.Client()
     resp = client.get(endpoint.build())
     df = ec.parse.as_dataframe(resp)
